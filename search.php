ea32961b971f7e49b43e853e767e6fa9401a367a
Peretz b <peretzb20@gmail.com>
2018-02-08 13:25:58 -0500

    make changes

--- search.php
+++ search.php
@@ -1,22 +1,29 @@
 <?php
-if(isset($POST["submit_button"])){
+if(isset($POST["submission"])){
     if(isset($GET["go"])){
-        if(preg_match("^/[ a-zA-Z]+/",$POST["search_query"])){
-            $searchQuery=$POST["search_query"];
-            $database=mysql_connect("servername","username","password") or die("Error connecting to database".mysql_error());
-            $ourDatabase=mysql_select_db("FrumToronto");
-            $sql="SELECT ID search_query FROM database WHERE search_query LIKE '%".$searchQuery;
-            $result=mysql_query($sql);
-            while($row=mysql_fetch_array($result)){
-                $ID=row["ID"];
-                $searchedText=row["search_query"];
+        if(preg_match("^/[ a-zA-Z]+/",$POST["query"])){
+            $searchQuery=$POST["query"];
+            $servername="LONDON2012\London2012";
+            $database=mssql_connect($servername,"frumto","201518");
+            if(!$database){
+                die("Error connecting to database");
+            }
+            $ourDatabase=mssql_select_db("[FrumShared].[dbo].[BlogEntries]",$database);
+            $sql="SELECT BlogEntryTitle, BlogEntryText FROM [FrumShared].[dbo].[BlogEntries] WHERE BlogCategoryID=98 AND BlogEntryTitle LIKE '%".$searchQuery;
+            $result=mssql_query($sql);
+            while($row=mssql_fetch_array($result)){
+                $blogEntryTitle=row["BlogEntryTitle"];
+                $blogEntryText=row["BlogEntryText"];
                 echo "<ul>\n";
-                echo "<li>"."<a href=\"search.php?id=\">".$searchQuery."</a></li>\n";
+                echo "<li>".$blogEntryTitle."</li>\n";
+                echo "<li>".$blogEntryText."</li>\n";
                 echo "</ul>";
             }
         }else{
             echo "<p>You need to enter a search query for this form to work</p>";
         }
     }
+}else{
+    echo "Error submitting data";
 }
 ?>
